ad7c9714177faa555bf032fd26214c1b2783d496
rlzijdeman <richard.zijdeman@iisg.nl>
2019-07-26 14:52:45 +0200

    added python script to retrieve data from endpoint

--- retrieve-query-example.py
+++ retrieve-query-example.py
@@ -1,8 +1,19 @@
+
+#import libraries
 import pandas as pd
 import requests
+import io
+
+# set info for retrieval
 url = "http://grlc.io/api/rlzijdeman/lp-sparql-tsv-queries/lp-cshapes-query?endpoint=https%3A%2F%2Fapi.druid.datalegend.net%2Fdatasets%2Fnlgis%2Fcshapes%2Fservices%2Fcshapes%2Fsparql"
 headers={'accept': 'text/csv'}
+
+# retrieve and format data
 resp = requests.get(url, headers = headers)
-# print(resp.status_code) # 200
-print(resp.content) # start with: b'"id", ...
-# df = pd.read_(resp.content)
+respClean = io.StringIO(resp.content.decode("utf-8"))
+df = pd.read_csv(respClean, sep = ",")
+
+# write to tsv file
+df.to_csv("./cshapes-whgz.tsv", sep="\t", index = False)
+
+# EOF
